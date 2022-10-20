2a57ccb5bf0a4b7b4fabf1e2423161cfa0133f5f
2haed <101021036+2haed@users.noreply.github.com>
2022-10-20 16:10:41 +0300

    project from home

--- graphes.py
+++ graphes.py
@@ -5,7 +5,6 @@ import vk_api
 from config import token
 import matplotlib.pyplot as plt
 
-
 session = vk_api.VkApi(token=token)
 vk = session.get_api()
 
@@ -14,20 +13,28 @@ def user_finder(user_id):
     return f"{vk.users.get(user_id=user_id)[0]['first_name']} {vk.users.get(user_id=user_id)[0]['last_name']}"
 
 
-def count_degree_centralities(G):
-    dictionary = nx.degree_centrality(G)
+def count_centrality(function) -> dict:
+    dictionary = function(G)
     sorted_tuples = sorted(dictionary.items(), key=operator.itemgetter(1), reverse=True)
-    return {k: v for k, v in sorted_tuples}
+    return {key: val for key, val in sorted_tuples}
+
+
+def get_all_centralities(G):
+    centralities_dict = count_centrality(lambda x: nx.degree_centrality(G))
+    closeness_centrality = count_centrality(lambda x: nx.closeness_centrality(G))
+    betweeness_centrality = count_centrality(lambda x: nx.betweenness_centrality(G))
+    eigenvector_centrality = count_centrality(lambda x: nx.eigenvector_centrality(G))
+    PageRank = count_centrality(lambda x: nx.pagerank(G))
+    print(f'Max degree centrality equals: {user_finder(next(iter(centralities_dict)))}')
+    print(
+        f'Degree centrality: {centralities_dict}\n'
+        f'Closeness centrality: {closeness_centrality}\nBetweeness centrality:{betweeness_centrality}\n'
+        f'Eigenvector centrality: {eigenvector_centrality}\nPage Rank: {PageRank}')
 
 
 with open('ready_data.json', 'r') as json_file:
     data = json.load(json_file)
-    for key, val in data.items():
-        data[key] = tuple(val)
-    G = nx.DiGraph(data)
-    centralities_dict = {key: str(round(val*100, 2))+'%' for key, val in count_degree_centralities(G).items()}
-    closeness_dict = nx.betweenness_centrality(G)
-    print(closeness_dict)
-    # print(f'Max degree centrality equals: {user_finder(next(iter(count_degree_centralities(G))))}')
-    # print(f'Degree centralities: {centralities_dict}')
+    data = {key: tuple(val) for key, val in data.items()}
 
+G = nx.DiGraph(data)
+get_all_centralities(G)
